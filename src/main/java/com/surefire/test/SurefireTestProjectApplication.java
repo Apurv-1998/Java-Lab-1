1080ebe61bb2ff3cb86cc5d2d45aefd5d5132724
Apurv Sirohi <31625651+Apurv-1998@users.noreply.github.com>
2023-02-07 17:31:47 +0530

    reports

---
+++ src/main/java/com/surefire/test/SurefireTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.surefire.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurefireTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurefireTestProjectApplication.class, args);
+	}
+
+}
